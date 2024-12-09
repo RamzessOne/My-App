68828f97e55bf693897d62803a703466ae3287bc
RamzessOne <semenchenkorv@gmail.com>
2024-12-10 01:17:44 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+
+    public static String shop;
+
+    public static void main(String[] args) {
+
+        shop = "This is shop-online app";
+        System.out.println(shop + " ");
+    }
+}
+
